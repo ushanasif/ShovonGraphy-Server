aa44011e6e91a2d0aa68dd5fb89124d5cd96fd1a
Asif Uddin Bhuiyan <ushanasif@gmail.com>
2025-06-19 20:16:12 +0600

    Update createJwToken.js

--- helpers/createJwToken.js
+++ helpers/createJwToken.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
 
+const createJwToken = (payload, jwtSecretKey, expiresIn) => {
+
+    if(typeof payload !== "object" || !payload){
+        throw new Error("Payload must be a non-empty object");
+    }
+    if(typeof jwtSecretKey !== "string" || jwtSecretKey === ""){
+        throw new Error("Secret key must be a non-empty string")
+    }
+
+    try {
+        const token = jwt.sign(payload, jwtSecretKey, {expiresIn});
+        return token;
+    } catch (error) {
+        console.log("Failed to sign the JWT", error);
+        throw error;
+    }
+}
+
+module.exports = { createJwToken };
