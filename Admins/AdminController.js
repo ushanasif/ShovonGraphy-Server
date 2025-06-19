4117828fb39e8c7c30872c58d5328e1fe13f919f
Asif Uddin Bhuiyan <ushanasif@gmail.com>
2025-06-19 20:06:33 +0600

    Update AdminController.js

--- Admins/AdminController.js
+++ Admins/AdminController.js
@@ -2,6 +2,7 @@ const AdminModel = require("./AdminModel");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { successResponse } = require("../helpers/successResponse");
+const { createJwToken } = require("../helpers/createJwToken");
 
 
 const adminRegister = async(req, res) => {
@@ -45,19 +46,21 @@ const adminLogin = async(req, res) => {
             id: adminExist._id
         }
         
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET_KEY, {
-            expiresIn: '30m'
-        });
+
+        const accessToken = createJwToken(payload, process.env.JWT_ACCESS_SECRET_KEY, '10m');
+
+        const refreshToken = createJwToken(payload, process.env.JWT_REFRESH_SECRET_KEY, '30d');
+
+        await AdminModel.findByIdAndUpdate(adminExist._id, {refreshToken});
         
-        res.cookie('accessToken', accessToken, {
-            maxAge: 1000 * 60 * 30, // 15 minutes
+        res.cookie('refreshToken', refreshToken, {
+            maxAge: 30 * 24 * 60 * 60 * 1000,
             httpOnly: true, // Prevent JavaScript access
-            secure: false,
-            sameSite: 'strict' // Prevent cross-site attacks
+            secure: process.env.NODE_ENV === 'production',
         });
 
 
-        res.status(200).json({success: true, message: "Admin is logged in successfully"})
+        res.status(200).json({success: true, message: "Admin is logged in successfully", accessToken, admin: adminExist})
     } catch (error) {
         res.status(500).json({error: true, message: error.message}); 
     }
@@ -66,8 +69,8 @@ const adminLogin = async(req, res) => {
 const getAdminDetail = async(req, res) => {
     try {
         
-        const adminExist = await AdminModel.findOne({_id: req.id});
-        console.log(adminExist);
+        const adminExist = await AdminModel.findOne({_id: req.id}).select("-password");
+       
         if(!adminExist){
             return res.status(400).json({error: true, message: "Please log in"})
         }
@@ -80,12 +83,41 @@ const getAdminDetail = async(req, res) => {
 
 const adminLogout = async(req, res) => {
     try {
-        res.clearCookie('accessToken');
+        const refreshToken = req?.cookies?.refreshToken;
+        res.clearCookie('refreshToken', {httpOnly: true, secure: process.env.NODE_ENV === 'production'});
+
+        await AdminModel.findOneAndUpdate({refreshToken}, {refreshToken: ''});
         return successResponse(res, 200, true, "Logout successful");
     } catch (error) {
         console.log(error.message);
         res.status(500).json({error: true, message: error.message});
     }
+};
+
+const refreshToken = async(req, res) => {
+    const refreshToken = req?.cookies?.refreshToken;
+    if(!refreshToken){
+        return res.status(401).json({message: 'Unauthorized!'})
+    }
+
+   try {
+    const refreshTokenExist = await AdminModel.findOne({refreshToken});
+    if(!refreshTokenExist){
+        return res.status(401).json({message: 'Unauthorized!'})
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY);
+    if(!decoded || decoded.id !== refreshTokenExist._id.toString()){
+        return res.status(401).json({ success: false, message: "Unauthorized here" });
+    }
+
+    const newToken = createJwToken({id: decoded.id}, process.env.JWT_ACCESS_SECRET_KEY, '10m');
+
+    res.status(200).json({accessToken: newToken});
+   } catch (error) {
+        res.status(500).json({error: true, message: error.message});
+   }
+
+
 }
 
-module.exports = {adminRegister, adminLogin, getAdminDetail, adminLogout};
+module.exports = {adminRegister, adminLogin, getAdminDetail, adminLogout, refreshToken};
