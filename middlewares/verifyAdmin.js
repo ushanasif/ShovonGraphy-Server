87061f02216a127fb51a9fd6ecf87cbf098a6927
Asif Uddin Bhuiyan <ushanasif@gmail.com>
2025-06-19 20:05:51 +0600

    Update verifyAdmin.js

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,21 +1,45 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const AdminModel = require("../Admins/AdminModel");
 
-const verifyAdmin = async(req, res, next) => {
-    try {
-        const accessToken = req?.cookies?.accessToken;
-        if(!accessToken){
-            return res.status(401).json({error: true, message: "Token not found"})
-        }
-        const decoded =  jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET_KEY);
-        if(!decoded){
-            return res.status(401).json({error: true, message: "Please sign in"})
-        }
-    
-        req.id = decoded.id;
-        next();
-    } catch (error) {
-        res.status(500).json({error: true, message: error.message});
-    }
-}
+const verifyToken = async (req, res, next) => {
+  const refreshToken = req?.cookies?.refreshToken;
+  const header = req?.headers?.authorization;
 
-module.exports = verifyAdmin;
+  const accessToken = header?.split(" ")[1];
+
+  if (!refreshToken) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  if (!accessToken) {
+    return res.status(401).json({ success: false, message: "Unauthorized!" });
+  } else {
+    jwt.verify(
+      accessToken,
+      process.env.JWT_ACCESS_SECRET_KEY,
+      async (err, decoded) => {
+        if (err) {
+          if (err.name === "TokenExpiredError") {
+            return res
+              .status(401)
+              .json({ success: false, message: "Unauthorized" });
+          }
+          return res
+            .status(401)
+            .json({ success: false, message: "Unauthorized" });
+        } else {
+          const isAdmin = await AdminModel.findOne({ _id: decoded.id });
+
+          if (!isAdmin) {
+            return res.status(401).json({ message: "UnAuthorized!" });
+          }
+
+          req.id = isAdmin?._id;
+          next();
+        }
+      }
+    );
+  }
+};
+
+module.exports = verifyToken;
